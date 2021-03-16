e80b04d7cb1eebce832db390c6254db3ca82f9ef
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-16 16:36:10 -0300

    Refactoring the router.js

--- crud_app/server/routes/router.js
+++ crud_app/server/routes/router.js
@@ -6,8 +6,10 @@ const services = require('../services/render')
 /* @description Root Route @method GET */
 route.get('/', services.homeRoutes)
 
+/* @description Add User @method GET */
 route.get('/', services.add_user)
 
+/* @description Update User @method GET */
 route.get('/', services.update_user)
 
 module.exports = route
